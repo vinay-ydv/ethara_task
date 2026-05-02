92c52e1ccd56c4310262b9d9b9754ffeb1c5fff6
vinay <vinayydv2003@gmail.com>
2026-05-02 11:15:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-     origin: "http://localhost:5173",
+     origin: "https://ethara-task-frontend.onrender.com",
      credentials: true 
 }))
 
